8a42c89e2015351e0b95000591cdbf76f36650f8
shrutikannan <kshruti@bu.edu>
2017-10-28 15:24:43 -0400

    Converted to function

--- block_motion.m
+++ block_motion.m
@@ -1,9 +1,4 @@
-clc;clear;close all;
-img1 = double(imread('missa_1.tif'));
-img2 = double(imread('missa_50.tif'));
-[rows, cols] = size(img1);
-N = 16; % Block size
-D = 8; % Search range, precision is 1
+function [min_d, vector_field, fig] = block_motion(img1, img2, rows, cols, N, D, error_fcn)
 
 min_d = zeros(D,1); %  Stores block start points and d vector for each
 b=1; % block number
@@ -23,16 +18,39 @@ for m = 1:N:(rows-D+1) % Loop over blocks
                 end
                 block1 = img1(m:m+N-1, n:n+N-1);
                 block2 = img2(m+d1:m+N-1+d1, n+d2:n+N-1+d2);
-                error(m+d1, n+d2) = sum(sum(abs((block1) - (block2)))); % mae %int16
-%                 error(m+d1, n+d2) = sum(sum((int16(block1) - int16(block2)).^2)); % mse
+                % if any 1 row or col is fully zeros, ignore that row/col
+                % in both blocks while computing error, it is probably the
+                % padded array
+                if any(any(block2,1) == 0) == 1
+                   x1 = find(any(block2,1) == 0);
+                   block1(:,x1) = [];
+                   block2(:,x1) = [];
+                elseif any(any(block1,1) == 0) == 1
+                   x1 = find(any(block1,1) == 0);
+                   block1(:,x1) = [];
+                   block2(:,x1) = [];
+                elseif any(any(block2,2) == 0) == 1
+                   x1 = find(any(block2,2) == 0);
+                   block1(x1,:) = [];
+                   block2(x1,:) = [];
+                elseif any(any(block2,2) == 0) == 1
+                   x1 = find(any(block2,2) == 0);
+                   block1(x1,:) = [];
+                   block2(x1,:) = [];
+                end                
+                if strcmp(error_fcn, 'mae')
+                    error(m+d1, n+d2) = sum(sum(abs(int16(block1) - int16(block2)))); % mae
+                elseif strcmp(error_fcn, 'mse')
+                    error(m+d1, n+d2) = sum(sum((int16(block1) - int16(block2)).^2)); % mse
+                end
             end
         end
-        ab1 = find(any(error,2),1,'last'); % Row of last element 
-        ab2 = find(any(error,1),1,'last'); % Col of last element
+        ab1 = find(any(error,2),1,'last'); % Row of last element of block in image
+        ab2 = find(any(error,1),1,'last'); % Col of last element of block in image
         error(~any(error,2), :) = [];  % Removing rows with 0s
         error(:, ~any(error,1)) = [];  % Removing columns with 0s
-        b1 = find(any(error,2),1,'last'); % Row of last element 
-        b2 = find(any(error,1),1,'last'); % Col of last element
+        b1 = find(any(error,2),1,'last'); % Row of last element in current block
+        b2 = find(any(error,1),1,'last'); % Col of last element in current block
         [~, tmp2] = min(error(:)); % min val; index
         if n+D >= cols-D+1
            new_centre_x = b1 - D;
@@ -44,82 +62,29 @@ for m = 1:N:(rows-D+1) % Loop over blocks
            new_centre_x = b1 - D;
            new_centre_y = b2 - D;
         end
-
-        min_d(b,1) = block_start_m; %m
-        min_d(b,2) = block_start_n; %n
-        [min_d(b,3), min_d(b,4)] = ind2sub(size(error()), tmp2);
+        min_d(b,1) = block_start_m; % Start row of the block
+        min_d(b,2) = block_start_n; % Start col of the block
+        [min_d(b,3), min_d(b,4)] = ind2sub(size(error()), tmp2); % Min d values
         min_d(b,3) = min_d(b,3) - new_centre_x;
         min_d(b,4)  = min_d(b,4) - new_centre_y;
-        min_d(b,5) = ab1;
-        min_d(b,6) = ab2;
-%         if b > 1
-%             d = min_d(b,2) - min_d(b-1,2)
-%             tmp1 = min_d(1:b-1, :)
-%             tmp3 = min_d(b,:) 
-%             tmp2 = repmat(min_d(b-1,:), [d-1,1])
-%             min_d = [tmp1; tmp2; tmp3];
-%             min_d
-%             b = d+1
-%         else
-            b = b+1;
-%         end
+        min_d(b,5) = ab1; % End row of block (for looping over blocks later)
+        min_d(b,6) = ab2; % End col of block (for looping over blocks later)
+        b = b+1;
     end
 end
-%%
-% 
-% figure;
-% imshow(img1);
-% figure;
-% imshow(img2);
-% figure;
-quiver(min_d(:,1), min_d(:,2), min_d(:,3), min_d(:,4))
-camroll(-90)
 
-%%
-
-for m = 1:length(min_d) % Loop over blocks
-    for row = min_d(m,1):min_d(m,5)
-        for col = min_d(m,2):min_d(m,6)
-            mp_error(row,col) = (img1(row,col)) - (img2(row+min_d(m,3), col+min_d(m,4))); %int16
-        end
-    end
+tmp = 1; % temporary variable to increment row number
+for i = 2:length(min_d) % Storing more points per block for plotting vector field
+    vector_field(tmp,1:4) = min_d(i,1:4);
+    vector_field(tmp+1,1) = min_d(i,1) + 4;
+    vector_field(tmp+1,2) = min_d(i,2);
+    vector_field(tmp+2,1) = min_d(i,1);
+    vector_field(tmp+2,2) = min_d(i,2) + 4;
+    vector_field(tmp+3,1:2) = min_d(i,1:2) + 4;
+    vector_field(tmp+1:tmp+3,3:4) = repmat(min_d(i,3:4),3,1);
+    tmp = tmp+4;
 end
-mp_error1 = mp_error > 0;
-mp_error1 = mp_error1 + 128 ;
-imshow(mp_error1)
-%%
-% 
-% 
 
-
-
-
-% start=2
-% for i = 2:length(min_d)
-%     no_rows = (min_d(i,2) - min_d(i-1,2));
-%     www(min_d(i-1,2),:) = min_d(i-1,:);
-%     for j = start: start+no_rows
-%         www(j,1) = min_d(i-1,1);
-%         www(j,2) = www(j-1,2) +1;
-%         www(j,3:4) = min_d(i-1,3:4);
-%         nxt = j;
-%     end
-%     if (min_d(i,1) ~= min_d(i-1,1))
-%         
-%     end
-%     www;
-%     start = min_d(i,2);
-%     
-% end
-%     
-
-
-
-%%
-% Loop over block centres m,n, shift by N
-    % Loop over each pixel in block, d1,d2, shift between -D to D 
-        % block1 is current image current block
-        % block2 is block1 shifted by d1,d2
-        % Calculate cost and store value, d1, d2
-    % d1, d2 associated with the lowest cost is to be found out, store i,j
-    % also
+fig = figure;
+quiver(vector_field(:,1), vector_field(:,2), vector_field(:,3), vector_field(:,4),0);
+camroll(-90);
